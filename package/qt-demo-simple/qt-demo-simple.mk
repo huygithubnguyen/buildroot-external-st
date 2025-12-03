cc7057e74c9ae05c35f8acf00c29272df1c35e0c
Huy Nguyen <realhuy35@gmail.com>
2025-12-03 14:37:01 +0700

    fix: wrong path toolchain build when using make clean

--- package/qt-demo-simple/qt-demo-simple.mk
+++ package/qt-demo-simple/qt-demo-simple.mk
@@ -8,6 +8,11 @@ QT_DEMO_SIMPLE_SOURCE = qt_demo_simple
 QT_DEMO_SIMPLE_DEPENDENCIES = qt5base json-c qt5-ipc-system
 
 QT_DEMO_SIMPLE_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+# Point CMake at the real external toolchain sysroot so builds survive `make clean`
+# even if the staging symlink is gone.
+QT_DEMO_SIMPLE_SYSROOT = $(HOST_DIR)/arm-buildroot-linux-gnueabihf/sysroot
+QT_DEMO_SIMPLE_CONF_OPTS += -DCMAKE_SYSROOT=$(QT_DEMO_SIMPLE_SYSROOT)
+QT_DEMO_SIMPLE_CONF_OPTS += -DCMAKE_FIND_ROOT_PATH=$(QT_DEMO_SIMPLE_SYSROOT)
 
 # Install to /opt/qt_demo for easy access
 QT_DEMO_SIMPLE_INSTALL_TARGET_OPTS = --prefix=/usr
